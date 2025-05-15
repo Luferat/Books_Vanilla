c7108d305c553885afad019165abcc152aec6671
André Luferat <contato@luferat.net>
2025-05-14 21:23:16 -0300

    Front da API

---
+++ src/main/java/com/books/api/controller/wwwController.java
@@ -0,0 +1,27 @@
+package com.books.api.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@RestController
+@RequiredArgsConstructor
+public class wwwController {
+
+    @GetMapping("/hello")
+    public String hello() throws IOException {
+        return getPage("src/main/resources/static/index.html");
+    }
+
+    public static String getPage(String path) throws IOException {
+        Path filePath = Paths.get(path);
+        return Files.readString(filePath);
+    }
+}
