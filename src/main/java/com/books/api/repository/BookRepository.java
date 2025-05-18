0e354e65909c35b8220132c73a2b6bdec5e855b0
Vitor Burnier <burnvitor2@gmail.com>
2025-05-18 01:26:07 -0300

    fix/ sort = Sort.by(direction, field);

--- src/main/java/com/books/api/repository/BookRepository.java
+++ src/main/java/com/books/api/repository/BookRepository.java
@@ -1,6 +1,7 @@
 package com.books.api.repository;
 
 import com.books.api.model.Book;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,7 +11,9 @@ import java.util.Optional;
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
     Optional<Book> findByTitle(String title);
+    List<Book> findByStatus(Book.Status status);
 
+    List<Book> findByStatus(Book.Status status, Sort sort);
 
     List<Book> findAllByOrderByTitleAsc();
 }
